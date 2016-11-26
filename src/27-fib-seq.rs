3ac6346a36739ad88f4bc1948bff0fb3e0b37cf1
Alexey Golubev <dr.freecx@gmail.com>
2016-11-26 11:02:34 +0300

    [add] simply realization of fibonacci number

---
+++ src/27-fib-seq.rs
@@ -0,0 +1,14 @@
+fn fib_seq(n: u32) -> u64 {
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    for _ in 0..n {
+        let c = b;
+        b = a + b;
+        a = c;
+    }
+    a
+}
+
+fn main() {
+    println!("fib_seq(5) = {}", fib_seq(5));
+}
